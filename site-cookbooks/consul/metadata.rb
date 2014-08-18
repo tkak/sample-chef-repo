4d8dbbdf15319e963b84cf79569eaaa05481b3f9
tkak <keepkeptkept@gmail.com>
2014-08-18 10:37:17 +0900

    change consul cookbook

---
+++ site-cookbooks/consul/metadata.rb
@@ -0,0 +1,7 @@
+name             'consul'
+maintainer       'tkak'
+maintainer_email 'keepkeptkept@gmail.com'
+license          'All rights reserved'
+description      'Install consul'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
--- site-cookbooks/terraform/recipes/default.rb
+++
@@ -1,6 +0,0 @@
-remote_file "/tmp/consul-linux-amd64.zip" do
-  source "https://dl.bintray.com/mitchellh/consul/0.3.1_linux_amd64.zip"
-end
-
-
-  
