db3bb38bc6182a3280f1386508030265f57ab822
tkak <takaaki.frkw@gmail.com>
2015-04-04 12:54:01 +0900

    Clean up

--- site-cookbooks/oh_my_zsh/metadata.rb
+++
@@ -1,6 +0,0 @@
-name              "oh_my_zsh"
-maintainer        "Takaaki Furukawa"
-license           "Apache 2.0"
-description       "Installs oh-my-zsh."
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
